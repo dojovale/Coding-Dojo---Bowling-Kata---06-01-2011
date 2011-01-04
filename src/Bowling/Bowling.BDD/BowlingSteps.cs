c62cfabfe68a781d7d71ddf75194786e20090433
Pepato <rpepato@gmail.com>
2011-01-04 09:20:06 -0200

    Inclusao de codigo fonte e libs de apoio

---
+++ src/Bowling/Bowling.BDD/BowlingSteps.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Bowling.BDD
+{
+    [Binding]
+    public class BowlingSteps
+    {
+        private Game game;
+
+        [Given(@"a new bowling game")]
+        public void GivenANewBowlingGame()
+        {
+            game = new Game();
+        }
+
+        [When(@"I roll the ball into the gutter 20 times")]
+        public void WhenIRollTheBallIntoTheGutter20Times()
+        {
+            DoRolls(20, 0);
+        }
+
+
+        [When(@"I do a strike in the first frame")]
+        public void WhenIDoAStrikeInTheFirstFrame()
+        {
+            DoStrike();
+        }
+
+        [When(@"Hit four pins in the next roll")]
+        public void WhenHitFourPinsInTheNextRoll()
+        {
+            game.Roll(4);
+        }
+
+        [Then(@"the score should be 24")]
+        public void ThenTheScoreShouldBe24()
+        {
+            Assert.AreEqual(24, game.Score);
+        }
+
+        [When(@"I do 12 consecutive strikes")]
+        public void WhenIDo12ConsecutiveStrikes()
+        {
+            DoRolls(12,10);
+        }
+
+        [Then(@"the score should be 300")]
+        public void ThenTheScoreShouldBe300()
+        {
+            Assert.AreEqual(300, game.Score);
+        }
+
+        [Then(@"the score should be 0")]
+        public void ThenTheScoreShouldBe0()
+        {
+            Assert.AreEqual(0, game.Score);
+        }
+
+        [When(@"I roll the ball 20 times and hit 1 pin per roll")]
+        public void WhenIRollTheBall20TimesAndHit1PinPerRoll()
+        {
+            DoRolls(20, 1);
+        }
+
+        [Then(@"the score should be 20")]
+        public void ThenTheScoreShouldBe20()
+        {
+            Assert.AreEqual(20, game.Score);
+        }
+
+        [When(@"I do a spare in the first frame")]
+        public void WhenIDoASpareInTheFirstFrame()
+        {
+            DoSpare();
+        }
+
+        [When(@"Hit tree pins in the next roll")]
+        public void WhenHitTreePinsInTheNextRoll()
+        {
+            game.Roll(3);
+        }
+
+        [When(@"throw the ball in the gutter on next 17 rolls")]
+        public void WhenThrowTheBallInTheGutterOnNext17Rolls()
+        {
+            DoRolls(17, 0);
+        }
+
+        [Then(@"the score should be 16")]
+        public void ThenTheScoreShouldBe16()
+        {
+            Assert.AreEqual(16, game.Score);
+        }
+
+        private void DoRolls(int numberOfRolls, int numberOfPins)
+        {
+            for (int rollNumber = 0; rollNumber < numberOfRolls; rollNumber++)
+            {
+                game.Roll(numberOfPins);
+            }
+        }
+
+        private void DoSpare()
+        {
+            game.Roll(3);
+            game.Roll(7);
+        }
+
+        private void DoStrike()
+        {
+            game.Roll(10);
+        }
+    }
+}
---
+++ src/Bowling/Bowling.BDD/BowlingTests.feature
@@ -0,0 +1,35 @@
+﻿Feature: Bowling Game Score
+	In order to focus on drop pins
+	As a bowling player
+	I want to be told of the score of my game
+
+@mytag
+Scenario: All Ones Game
+	Given a new bowling game	
+	When I roll the ball 20 times and hit 1 pin per roll  
+	Then the score should be 20
+
+Scenario: Gutter Game
+	Given a new bowling game	
+	When I roll the ball into the gutter 20 times  
+	Then the score should be 0
+
+Scenario: Spare In First Frame
+	Given a new bowling game	
+	When I do a spare in the first frame
+	And Hit tree pins in the next roll
+	And throw the ball in the gutter on next 17 rolls  
+	Then the score should be 16
+
+Scenario: Strike In First Frame
+	Given a new bowling game	
+	When I do a strike in the first frame
+	And Hit tree pins in the next roll
+	And Hit four pins in the next roll
+	And throw the ball in the gutter on next 17 rolls  
+	Then the score should be 24
+
+Scenario: Perfect Game
+	Given a new bowling game	
+	When I do 12 consecutive strikes	
+	Then the score should be 300
